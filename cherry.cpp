bf70621b5a7d1270ae35ddb28627f6a6b91381fb
Limaye91 <kasturilimaye19@gmail.com>
2024-01-08 18:40:02 +0530

    adding third line

--- cherry.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    cout << "this is cherry pick file";
-    cout << "adding next commit which should be added in feature2";
-    return 0;
-}
